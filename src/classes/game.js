49f021909499547b2a6287e405ae5579a9cb6de2
03jordan11 <jordanburnes@gmail.com>
2022-07-14 18:10:36 -0500

    added comments

--- src/classes/game.js
+++ src/classes/game.js
@@ -1,6 +1,5 @@
 
 import * as three from 'three'
-import { Vector3 } from 'three';
 import {OBJLoader} from 'three/examples/jsm/loaders/OBJLoader'
 import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls'
 
@@ -8,32 +7,43 @@ import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls'
 
 export default class Game {
     constructor(){
+        //Basic setup and CAMERA
         this.scene = new three.Scene();
         this.camera = new three.PerspectiveCamera(90, window.innerWidth / window.innerHeight, 0.1, 1000);
         this.renderer = new three.WebGLRenderer();
         this.renderer.setSize(window.innerWidth, window.innerHeight);
-        this.loader = new OBJLoader();
-
-        document.body.appendChild(this.renderer.domElement)
-
-        let geometry = new three.BoxGeometry(1, 1, 1);
-        let meterial = new three.MeshBasicMaterial( { color: 0x00ff00 });
-        this.cube = new three.Mesh(geometry, meterial);
-        this.axesHelper = new three.AxesHelper( 5 );
-        this.scene.add(this.axesHelper);
-        //this.scene.add(this.cube);
         this.camera.position.z = 25;
         this.camera.position.y = 15;
         this.camera.position.x = -10;
 
+        //Initialize OBJLoader
+        this.loader = new OBJLoader();
 
+        //Creates canvas on page
+        document.body.appendChild(this.renderer.domElement)
+
+        //TODELETE basic example using a cube, should be done outsid constructor and called by a function
+        let geometry = new three.BoxGeometry(1, 1, 1);
+        let meterial = new three.MeshBasicMaterial( { color: 0x00ff00 });
+        this.cube = new three.Mesh(geometry, meterial);
+        //this.scene.add(this.cube);
+
+        //Show's axis for project, should only be active in dev environment
+        this.axesHelper = new three.AxesHelper( 5 );
+        this.scene.add(this.axesHelper);
+
+
+
+        //Allows you to orbit around the camera
         this.controls = new OrbitControls(this.camera, this.renderer.domElement)
 
+        //Adding light so that we can see objects
         this.light = new three.PointLight(0xff0000, 10, 100);
         this.light.position.set(50, 50, 50);
         this.scene.add(this.light);
     }
 
+    //Call this to begin the game loop
     render = () => {
         requestAnimationFrame(this.render);
 
@@ -46,7 +56,7 @@ export default class Game {
     }
 
 
-
+    //For loading OBJ files TODO: need to figure out textures
     loadObjects = () => {
         const result = new Promise((resolve, reject) => {
             this.loader.load('../../assets/wooden_model _truck.obj', 
